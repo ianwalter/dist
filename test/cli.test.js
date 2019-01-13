47691d7b489af213b6485f70cc95aee68e232587
Ian Walter <public@iankwalter.com>
2019-01-12 22:30:04 -0500

    Adding plugins option test

--- test/cli.test.js
+++ test/cli.test.js
@@ -4,7 +4,7 @@ import execa from 'execa'
 
 const cli = './cli.js'
 
-test(`cli doesn't generate cjs file when passed --no-cjs`, async t => {
+test(`cli doesn't generate cjs file when passed --no-cjs flag`, async t => {
   const input = 'test/fixtures/exportDefaultFunction.js'
   const esm = 'tmp/one.js'
   const { stdout } = await execa(cli, ['-i', input, '--esm', esm, '--no-cjs'])
@@ -16,6 +16,15 @@ test(`cli transpiles dist file when given babel flag`, async t => {
   const input = 'test/fixtures/exportDefaultNewExpression.js'
   const cjs = 'tmp/two.js'
   const { stdout } = await execa(cli, ['-i', input, '--cjs', cjs, '--babel'])
+  t.snapshot(stdout)
+  t.snapshot(readFileSync(cjs, 'utf8'))
+})
+
+test(`cli produces dist file using plugins flag`, async t => {
+  const input = 'test/fixtures/exportObjectWithVueComponent.js'
+  const cjs = 'tmp/three.js'
+  const plugins = ['--plugins', 'test/helpers/vuePlugin.js']
+  const { stdout } = await execa(cli, ['-i', input, '--cjs', cjs, ...plugins])
   t.snapshot(stdout)
   t.snapshot(readFileSync(cjs, 'utf8'))
 })
