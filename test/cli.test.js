3548fc8998747a856fd97746f48b46b26a3ba628
Ian Walter <public@iankwalter.com>
2019-01-12 22:23:20 -0500

    Fixing cli tests

--- test/cli.test.js
+++ test/cli.test.js
@@ -1,3 +1,4 @@
+import { readFileSync } from 'fs'
 import test from 'ava'
 import execa from 'execa'
 
@@ -6,13 +7,15 @@ const cli = './cli.js'
 test(`cli doesn't generate cjs file when passed --no-cjs`, async t => {
   const input = 'test/fixtures/exportDefaultFunction.js'
   const esm = 'tmp/one.js'
-  const { stdout } = await execa(cli, [input, '--esm', esm, '--no-cjs'])
+  const { stdout } = await execa(cli, ['-i', input, '--esm', esm, '--no-cjs'])
   t.snapshot(stdout)
+  t.snapshot(readFileSync(esm, 'utf8'))
 })
 
 test(`cli transpiles dist file when given babel flag`, async t => {
   const input = 'test/fixtures/exportDefaultNewExpression.js'
   const cjs = 'tmp/two.js'
-  const { stdout } = await execa(cli, [input, '--cjs', cjs, '--babel'])
+  const { stdout } = await execa(cli, ['-i', input, '--cjs', cjs, '--babel'])
   t.snapshot(stdout)
+  t.snapshot(readFileSync(cjs, 'utf8'))
 })
